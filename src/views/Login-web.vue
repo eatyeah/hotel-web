61fbdc89ed5c8eba9c5cc7563a2b41c3485551c2
sweeterjava <sweeterjava@163.com>
2022-10-29 10:30:41 +0800

    上传项目所需要的图片

---
+++ src/views/Login-web.vue
@@ -0,0 +1,27 @@
+<!--客户端登录-->
+<template>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+
+      },
+      rules: {
+
+      }
+    };
+  },
+
+}
+</script>
+
+<style>
+body {
+  background: #66B1FF;
+}
+
+</style>
